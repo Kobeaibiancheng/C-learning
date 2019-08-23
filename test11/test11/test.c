d6ed530b4cc3cfb0bb7f7c9767bafedf716859e6
kobeaibiancheng <wyx160413@yeah.net>
2019-08-23 15:41:18 +0800

    练习

--- test11/test11/test.c
+++ test11/test11/test.c
@@ -42,29 +42,61 @@
 //	return 0;
 //}
 
-char* MyStrcpy(char* dest, const char* str)
-{
-	char* arr = dest;
-	assert(dest != NULL);
-	assert(str != NULL);
-	//while (*str != '\0')
-	//{
-	//	*dest = *str;
-	//	dest++;
-	//	str++;
-	//}
-	//*dest = *str;
-	//return arr;
-	while (*dest++ = *str++)  //先解引用，然后赋值，再++；
-	{                        //直到将'\0'赋值，'\0'的ASCII值是0，循环结束
-		;
-	}
-	return arr;
-}
+//char* MyStrcpy(char* dest, const char* str)
+//{
+//	char* arr = dest;
+//	assert(dest != NULL);
+//	assert(str != NULL);
+//	//while (*str != '\0')
+//	//{
+//	//	*dest = *str;
+//	//	dest++;
+//	//	str++;
+//	//}
+//	//*dest = *str;
+//	//return arr;
+//	while (*dest++ = *str++)  //先解引用，然后赋值，再++；
+//	{                        //直到将'\0'赋值，'\0'的ASCII值是0，循环结束
+//		;
+//	}
+//	return arr;
+//}
+//int main()
+//{
+//	char arr1[10] = { 0 };
+//	char arr2[] = "hello";
+//	printf("%s\n", MyStrcpy(arr1, arr2));
+//	return 0;
+//}
+
+
+//A选手说：B第二，我第三； 
+//B选手说：我第二，E第四；
+//C选手说：我第一，D第二；
+//D选手说：C最后，我第三；
+//E选手说：我第四，A第一；
+//比赛结束后，每位选手都说对了一半，请编程确定比赛的名次。
 int main()
 {
-	char arr1[10] = { 0 };
-	char arr2[] = "hello";
-	printf("%s\n", MyStrcpy(arr1, arr2));
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	for (a = 1; a <= 5; a++)
+		for (b = 1; b <= 5; b++)
+			for (c = 1; c <= 5; c++)
+				for (d = 1; d <= 5; d++)
+					for (e = 1; e <= 5; e++)
+					{
+		if (((b == 1) + (a == 3) == 1) &&
+			((b == 2) + (e == 4) == 1) &&
+			((c == 1) + (d == 2) == 1) &&
+			((c == 5) + (d == 3) == 1) &&
+			((e == 4) + (a == 1) == 1) == 1)
+		{
+			printf("a=%d b=%d c=%d d=%d e=%d \n", a, b, c, d, e);
+		}
+					}
 	return 0;
 }
--- test12/test12/test.c
+++ test12/test12/test.c
@@ -25,9 +25,74 @@
 //	return 0;
 //}
 
-int main()
-{
-	char arr[] = "student a am i";
-	ChangeString(arr);
-	return 0;
-}
+//指针访问二级数组的元素,形参是 指针数组
+//void Print(int(*parr)[5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			//parr指针变量中存放arr[3][5]第一行的地址，parr+i表示第i行的地址
+//			//*(parr+i)访问第i行的内容，(*(parr+i)+j)访问第i行第j列的元素
+//			printf("%d ", *((*parr+i)+j));
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1, 2, 3, 4, 5, 2, 3, 4, 5, 6, 3, 4, 5, 6, 7 };
+//	Print(arr, 3, 5);
+//	return 0;
+//}
+
+//访问二级数组，用二级数组接收
+//void Print(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1, 2, 3, 4, 5, 2, 3, 4, 5, 6, 3, 4, 5, 6, 7 };
+//	Print(arr, 3, 5);
+//	return 0;
+//}
+//void Print(char (*parr)[7])
+//{
+//	printf("%s\n", parr);
+//}
+//int main()
+//{
+//	char arr[] = "abcedf";
+//	Print(&arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	int (*p)[10] = &arr;
+	//for (i = 0; i < sz; i++)
+	//{
+	//	printf("%d ", (*p)[i]);
+	////}
+	//int* p = arr;
+	//for (i = 0; i < sz; i++)
+	//{
+	//	printf("%d ", *(p + i));
+	//}
+//	return 0;
+//}
