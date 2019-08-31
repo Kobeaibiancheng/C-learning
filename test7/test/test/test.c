5029d11ee48f94cec4e2242e951034ee4abcc659
kobeaibiancheng <wyx160413@yeah.net>
2019-08-31 17:47:45 +0800

    练习

---
+++ test7/test/test/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int main()
+//{
+//	int a = 5;
+//	//a>>1
+//	//00000000000000000000000000000101
+//	// 00000000000000000000000000000101
+//	return 0;
+//}
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	return 0;
+}
