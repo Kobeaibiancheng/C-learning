1e80527ef12f0a231a226f07421c35bbd5d507f8
kobeaibiancheng <wyx160413@yeah.net>
2019-09-07 21:40:44 +0800

    练习

--- 实现字符串操作函数/实现字符串操作函数/test.c
+++ 实现字符串操作函数/实现字符串操作函数/test.c
@@ -4,46 +4,49 @@
 #include<stdio.h>
 
 
-char* MyStrcpy(char* dest, const char* src)
-{
-	char* str = dest;
-	assert(dest&&src);
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return str;
+//char* MyStrcpy(char* dest, const char* src)
+//{
+//	char* str = dest;
+//	assert(dest&&src);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return str;
+//
+//}
+//
+//
+//char* MyStrcat(char* dest, const char* src)
+//{
+//	char* str = dest;
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return str;
+//}
+//
+//
+//int MyStrcmp(const char* str1, const char* str2)
+//{
+//	assert(str1&&str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//			return 0;
+//		str1++;
+//		str2++;
+//	}
+//	return *str1 - *str2;
+//}
 
-}
 
 
-char* MyStrcat(char* dest, const char* src)
-{
-	char* str = dest;
-	while (*dest)
-	{
-		dest++;
-	}
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return str;
-}
-
-
-int MyStrcmp(const char* str1, const char* str2)
-{
-	assert(str1&&str2);
-	while (*str1 == *str2)
-	{
-		if (*str1 == '\0')
-			return 0;
-		str1++;
-		str2++;
-	}
-	return *str1 - *str2;
-}
 int main()
 {
 	//char arr[20] = { 0 };
