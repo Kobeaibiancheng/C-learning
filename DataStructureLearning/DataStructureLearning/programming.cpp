9a1a18662eaa188557f3552459ad52d3bc1b2a5c
kobeaibiancheng <wyx160413@yeah.net>
2021-01-26 20:29:14 +0800

    test

---
+++ DataStructureLearning/DataStructureLearning/programming.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+#define max 1005
+struct Node{
+	int x;
+	int y;
+};
+Node arr[max];
+int main() {
+	int n = 0;
+	int five[5] = { 0 };
+	scanf("%d", &n);
+	for (size_t i = 0; i < n; i++)
+	{
+		arr[i].x = scanf("%d", &arr[i].x);
+		arr[i].y = scanf("%d", &arr[i].y);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int count = 0;
+		for (int j = 0; j < n; j++)
+		{
+			if (arr[i].x + 1 == arr[j].x)
+
+		}
+	}
+
+	return 0;
+}
