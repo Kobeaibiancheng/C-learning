31ac5650cdf4c358a3a3f682fb2251d11eb4e123
kobeaibiancheng <wyx160413@yeah.net>
2021-06-22 12:45:25 +0800

    study

---
+++ DataStructureLearning/DataStructureLearning/main.cpp
@@ -0,0 +1,21 @@
+//#include"MyQueue.c"
+//
+//#include<stdio.h>
+//
+//int main(){
+//	SqQueue Q;
+//	InitQueue(Q);
+//	//bool ret = QueueFill(Q);
+//
+//	InQueue(Q, 1);
+//	InQueue(Q, 2);
+//	InQueue(Q, 3);
+//	InQueue(Q, 4);
+//	int x = -1;
+//	DeQueue(Q, x);
+//	DeQueue(Q, x);
+//	DeQueue(Q, x);
+//	DeQueue(Q, x);
+//	return 0;
+//	return 0;
+//}
